2d747388ef8b443c0524eaba1bd8e108dc332f80
chedieck <estevao@lobo.dev.br>
2023-10-21 12:57:16 -0300

    refactor: better name

--- src/scripts/tomp3.py
+++ src/scripts/tomp3.py
@@ -12,10 +12,15 @@ lang = sys.argv[2]
 with open(textfile, "r") as f:
     text = f.read()
 
-def get_mili():
-    return math.floor(time.time())
+def get_name():
+    mili = math.floor(time.time() * 1000)
+    beginning = text.split()[0]
+    if len(beginning) > 10:
+        beginning = beginning[:10]
+    return f'{beginning}_{mili}.mp3'
 
 g = gTTS(text, lang=lang)
-g.save(f'download/{get_mili()}.mp3')
+mp3_name = get_name()
+g.save(f'download/{mp3_name}')
 
-print ('file saved to', f'download/{get_mili()}.mp3')
+print (f'download/{get_name()}.mp3')
