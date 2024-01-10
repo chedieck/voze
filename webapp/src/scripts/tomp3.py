b2b4b6ab0249527ab2353520112fd24dd6cde8a1
chedieck <estevao@lobo.dev.br>
2024-01-10 12:18:22 -0300

    feat: using bark

--- webapp/src/scripts/tomp3.py
+++ webapp/src/scripts/tomp3.py
@@ -1,9 +1,12 @@
 import sys
-from gtts import gTTS
 import time
 import math
 import string
 import json
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+from IPython.display import Audio
+
 
 with open('src/config.json', 'r') as file:
     config = json.load(file)
@@ -14,6 +17,7 @@ if len(sys.argv) < 3:
 
 textfile = sys.argv[1]
 lang = sys.argv[2]
+text = 'Example text'
 
 with open(textfile, "r") as f:
     text = f.read()
@@ -28,8 +32,17 @@ def get_name():
     beginning = ''.join([c for c in beginning if c not in string.punctuation])
     return f'downloads/{beginning}_{mili}.mp3'
 
-g = gTTS(text, lang=lang)
 mp3_name = get_name()
 
-g.save(mp3_name)
-print (mp3_name)
+print(mp3_name)
+
+# download and load all models
+preload_models()
+
+audio_array = generate_audio(text)
+
+# save audio to disk
+write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
+  
+# play text in notebook
+Audio(audio_array, rate=SAMPLE_RATE)
